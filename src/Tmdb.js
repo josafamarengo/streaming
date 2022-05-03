28a91e1afc9260a069646842db6573ae9eb1ae70
josafamarengo <josafabmarengo@gmail.com>
2022-05-03 17:53:41 -0300

    modified:   src/Tmdb.js

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -14,32 +14,32 @@ export default {
             {
                 slug: 'originals',
                 title: 'Recomendados para você',
-                items: await basicFetch(`/discover/tv?with_networks=213=language=pt-BR&api_key=${API_KEY}&sort_by=vote_average.desc&vote_count.gte=500`)
+                items: await basicFetch(`/discover/tv?with_networks=213=language=pt-BR&api_key=${API_KEY}&sort_by=vote_average.desc&vote_count.gte=2000`)
             },
             {
                 slug: 'action',
                 title: 'Ação',
-                items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}&sort_by=vote_average.desc&vote_count.gte=100`)
+                items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}&sort_by=vote_average.desc&vote_count.gte=15000`)
             },
             {
                 slug: 'horror',
                 title: 'Terror',
-                items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}&sort_by=vote_average.desc&vote_count.gte=100`)
+                items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}&sort_by=vote_average.desc&vote_count.gte=2000`)
             },
             {
                 slug: 'comedy',
                 title: 'Comédia',
-                items: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}&sort_by=vote_average.desc&vote_count.gte=100`)
+                items: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}&sort_by=vote_average.desc&vote_count.gte=10000`)
             },
             {
                 slug: 'romance',
                 title: 'Romance',
-                items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}&sort_by=vote_average.desc&vote_count.gte=100`)
+                items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}&sort_by=vote_average.desc&vote_count.gte=2000`)
             },
             {
                 slug: 'documentary',
                 title: 'Documentários',
-                items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}&sort_by=vote_average.desc&vote_count.gte=100`)
+                items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}&sort_by=vote_average.desc&vote_count.gte=200`)
             },
         ];
     },
